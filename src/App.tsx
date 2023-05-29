ae4a8aafacc56ee65a4ce899c738ba47181a3c95
imotai <codego.me@gmail.com>
2023-05-29 18:10:05 +0800

    feat: add ui layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,118 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import ProTip from './ProTip';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
-function Copyright() {
+function Copyright(props: any) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}.
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://db3.network/">
+        DB3 Network
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
     </Typography>
   );
 }
 
-export default function App() {
+const defaultTheme = createTheme();
+export default function SignIn() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Material UI Create React App example in TypeScript
-        </Typography>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar
+            sx={{ m: 1, bgcolor: "secondary.main" }}
+            src="https://s1.ax1x.com/2023/05/27/p9LFTTP.jpg"
+          ></Avatar>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <Typography variant="h6" gutterBottom align="center">
+                $100 DB3
+              </Typography>
+              <Typography
+                variant="caption"
+                display="block"
+                gutterBottom
+                align="center"
+              >
+                Total Funded Amount
+              </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant="h6" gutterBottom align="center">
+                1000
+              </Typography>
+
+              <Typography
+                variant="caption"
+                display="block"
+                gutterBottom
+                align="center"
+              >
+                Total Funded Address
+              </Typography>
+            </Grid>
+          </Grid>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <ConnectButton
+              showBalance={false}
+              label="Connect to Wallet"
+              accountStatus="address"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Request Facuet
+            </Button>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,53 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
-import App from './App';
-import theme from './theme';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import App from "./App";
+import theme from "./theme";
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  Chain,
+  lightTheme,
+} from "@rainbow-me/rainbowkit";
 
-const rootElement = document.getElementById('root');
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { scrollTestnet } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement!);
+
+const { chains, publicClient } = configureChains(
+  [scrollTestnet],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "DB3 Network Bridge",
+  projectId: "DB3 Network",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
 
 root.render(
   <ThemeProvider theme={theme}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
-    <App />
-  </ThemeProvider>,
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider
+        chains={chains}
+        theme={lightTheme({ borderRadius: "small" })}
+      >
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  </ThemeProvider>
 );
